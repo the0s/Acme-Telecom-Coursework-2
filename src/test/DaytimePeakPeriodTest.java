1763520d495c89d78bd36da7a8d8d2bac6a54d41
Theo <theodosis8@hotmail.com>
2011-12-03 20:13:37 +0000

    first test

--- src/test/DaytimePeakPeriodTest.java
+++ src/test/DaytimePeakPeriodTest.java
@@ -23,27 +23,27 @@ public class DaytimePeakPeriodTest {
 
 
     @Test
-	public void checkForNonPeakTime() throws ParseException {
+	public void checkForPeakTime() throws ParseException {
         DaytimePeakPeriod daytimePeakPeriod = new DaytimePeakPeriod();
         SimpleDateFormat dfm = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        Date nonPeakDate = dfm.parse("2012-12-03 18:15:00");
-        assertFalse(daytimePeakPeriod.offPeak(nonPeakDate));
+        Date PeakDate = dfm.parse("2012-12-03 18:15:00");
+        assertFalse(daytimePeakPeriod.offPeak(PeakDate));
     }
 
     @Test
-	public void checkForPeakTimeMorning() throws ParseException {
+	public void checkForNonPeakTimeMorning() throws ParseException {
         DaytimePeakPeriod daytimePeakPeriod = new DaytimePeakPeriod();
         SimpleDateFormat dfm = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        Date peakDate = dfm.parse("2012-12-03 06:15:00");
-        assertTrue(daytimePeakPeriod.offPeak(peakDate));
+        Date nonPeakDate = dfm.parse("2012-12-03 06:15:00");
+        assertTrue(daytimePeakPeriod.offPeak(nonPeakDate));
     }
 
      @Test
 	public void checkForPeakTimeEvening() throws ParseException {
         DaytimePeakPeriod daytimePeakPeriod = new DaytimePeakPeriod();
         SimpleDateFormat dfm = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        Date peakDate = dfm.parse("2012-12-03 19:15:00");
-        assertTrue(daytimePeakPeriod.offPeak(peakDate));
+        Date nonPeakDate = dfm.parse("2012-12-03 19:15:00");
+        assertTrue(daytimePeakPeriod.offPeak(nonPeakDate));
     }
 
 
