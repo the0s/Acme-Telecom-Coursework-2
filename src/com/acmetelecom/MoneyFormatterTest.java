58b2f33c8eff5543b533cf3bfe4fc93915292d50
Andreas Violaris <andreasv1989@gmail.com>
2011-12-03 20:55:08 +0000

    test

---
+++ src/com/acmetelecom/MoneyFormatterTest.java
@@ -0,0 +1,24 @@
+package com.acmetelecom;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import static junit.framework.Assert.assertTrue;
+
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Andreas
+ * Date: 03/12/11
+ * Time: 20:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class MoneyFormatterTest {
+    @Test
+    public void testPenceToPounds() throws Exception {
+        BigDecimal pence = new BigDecimal(100);
+        String pounds = MoneyFormatter.penceToPounds(pence);
+        assertTrue(pounds.equals("1.00"));
+    }
+}
