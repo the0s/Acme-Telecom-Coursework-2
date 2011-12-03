8c6fd837e9b52c5646cac26b436f665750f64a49
Theo <theodosis8@hotmail.com>
2011-12-03 18:59:04 +0000

    giasou giorgo

---
+++ src/test/DaytimePeakPeriodTest.java
@@ -0,0 +1,23 @@
+package test;
+
+
+import com.acmetelecom.DaytimePeakPeriod;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: The0s
+ * Date: 03/12/11
+ * Time: 18:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class DaytimePeakPeriodTest {
+    DaytimePeakPeriod daytimePeakPeriod = new DaytimePeakPeriod();
+
+    Date peakDate = Date('');
+
+    @Test
+	public void reportsWhenFirstLocationDiscovered() {
+    }
