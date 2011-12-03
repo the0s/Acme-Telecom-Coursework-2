3a774bb01b10063bb078b1a34a71b39b52ade62e
Theo <theodosis8@hotmail.com>
2011-12-03 22:07:00 +0000

    interface on billing system + new setters

--- src/com/acmetelecom/BillingSystem.java
+++ src/com/acmetelecom/BillingSystem.java
@@ -2,12 +2,10 @@ package com.acmetelecom;
 
 public class BillingSystem extends AbstractBillingSystem {
 
-    @Override
     public void callInitiated(String caller, String callee) {
         callLog.add(new CallStart(caller, callee));
     }
 
-    @Override
     public void callCompleted(String caller, String callee) {
         callLog.add(new CallEnd(caller, callee));
     }
