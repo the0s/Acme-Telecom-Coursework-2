5073da804b075aabdf3ce96079b00a47f5e21a06
Theo <theodosis8@hotmail.com>
2011-12-04 00:10:51 +0000

    testing

--- src/com/acmetelecom/LineItem.java
+++ src/com/acmetelecom/LineItem.java
@@ -10,27 +10,27 @@ import java.math.BigDecimal;
  * To change this template use File | Settings | File Templates.
  */
 public class LineItem {
-        private Call call;
-        private BigDecimal callCost;
+    private Call call;
+    private BigDecimal callCost;
 
-        public LineItem(Call call, BigDecimal callCost) {
-            this.call = call;
-            this.callCost = callCost;
-        }
-
-        public String date() {
-            return call.date();
-        }
-
-        public String callee() {
-            return call.callee();
-        }
-
-        public String durationMinutes() {
-            return "" + call.durationSeconds() / 60 + ":" + String.format("%02d", call.durationSeconds() % 60);
-        }
-
-        public BigDecimal cost() {
-            return callCost;
-        }
+    public LineItem(Call call, BigDecimal callCost) {
+        this.call = call;
+        this.callCost = callCost;
     }
+
+    public String date() {
+        return call.date();
+    }
+
+    public String callee() {
+        return call.callee();
+    }
+
+    public String durationMinutes() {
+        return "" + call.durationSeconds() / 60 + ":" + String.format("%02d", call.durationSeconds() % 60);
+    }
+
+    public BigDecimal cost() {
+        return callCost;
+    }
+}
