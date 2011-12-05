d7c4c89ea04cd79820d94464a2bb56d5ce43818d
Theo <theodosis8@hotmail.com>
2011-12-05 18:19:22 +0000

    eeeeeeeeeeee malaka

--- src/com/acmetelecom/ProductionFactory.java
+++ src/com/acmetelecom/ProductionFactory.java
@@ -1,11 +1,12 @@
 package com.acmetelecom;
 
 import com.acmetelecom.billingsystems.*;
+import com.acmetelecom.billingsystems.billinggenerators.BillGenerator;
 
 public class ProductionFactory extends AbstractFactory {
 	
 
     public AbstractBillingSystem createBillingSystem(BillGeneratorInterface billGenerator, Logger logger, Report report) {
-        return new BillingSystem(billGenerator,logger,report);
+        return new BillingSystem(new BillGenerator(),logger,report);
     }
 }
--- src/com/acmetelecom/TestFactory.java
+++ src/com/acmetelecom/TestFactory.java
@@ -2,14 +2,15 @@ package com.acmetelecom;
 
 import com.acmetelecom.billingsystems.AbstractBillingSystem;
 import com.acmetelecom.billingsystems.BillGeneratorInterface;
+import com.acmetelecom.billingsystems.Logger;
+import com.acmetelecom.billingsystems.Report;
 import com.acmetelecom.test.com.acmetelecom.fake.BillGeneratorFake;
 import com.acmetelecom.test.com.acmetelecom.fake.BillingSystemFake;
 
 public class TestFactory extends AbstractFactory {
 	
-	public AbstractBillingSystem createBillingSystem(){
-		BillGeneratorInterface billGenerator= new BillGeneratorFake();
-		return new BillingSystemFake(billGenerator);
+	public AbstractBillingSystem createBillingSystem(BillGeneratorInterface billGenerator, Logger logger, Report report){
+		return new BillingSystemFake(new BillGeneratorFake(),logger,report);
 	}
 
 	
