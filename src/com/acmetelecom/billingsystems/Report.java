553f0b54d4a4441343b5056615c8b71c9f37fcea
Theo <theodosis8@hotmail.com>
2011-12-05 17:35:24 +0000

    local test.txt printer is history

---
+++ src/com/acmetelecom/billingsystems/Report.java
@@ -0,0 +1,11 @@
+package com.acmetelecom.billingsystems;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: The0s
+ * Date: 05/12/11
+ * Time: 17:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class Report {
+}
---
+++ src/com/acmetelecom/billingsystems/reports/BillReport.java
@@ -0,0 +1,11 @@
+package com.acmetelecom.billingsystems.reports;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: The0s
+ * Date: 05/12/11
+ * Time: 16:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class BillReport {
+}
